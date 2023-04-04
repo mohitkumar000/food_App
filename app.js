473bde9521779d2d5808393bafaf7adcee5392ea
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2023-04-05 01:31:06 +0530

    hashing update delete

--- app.js
+++ app.js
@@ -1,165 +1,157 @@
 const express = require("express");
 const app = express();
-const mongoose=require('mongoose');
-//middleware func-> post, front-> json
-app.use(express.json()); //global middleware 
-app.listen(3000);
+const userModel=require('./models/userModel')
+app.use(express.json());
 
-// let users = [
-//   {
-//     id: 1,
-//     name: "Abhishek",
-//   },
-//   {
-//     id: 2,
-//     name: "Jasbir",
-//   },
-//   {
-//     id: 3,
-//     name: "Kartik",
-//   },
-// ];
-//mini app
+let user = [
+  {
+    id: 1,
+    name: "Abhishek",
+    age: 100,
+  },
+  {
+    id: 2,
+    name: "Rajat",
+    age: 10,
+  },
+  {
+    id: 3,
+    name: "Sunjyot",
+    age: 50,
+  },
+];
+
 const userRouter = express.Router();
-const authRouter=express.Router();
-//base route , router to use
+const authRouter = express.Router();
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
 
 userRouter
   .route("/")
-  .get(getUsers) //path specific middleware
+//   .get(middleware1,getUser,middleware2)
+  .get(middleware1,getUsers)
   .post(postUser)
   .patch(updateUser)
   .delete(deleteUser);
 
-userRouter.route("/:id").get(getUserById);
+userRouter.route("/:name").get(getUserById);
 
-authRouter
-.route('/signup')
-.get(middleware1,getSignUp,middleware2)
-.post(postSignUp);
+authRouter.route("/signup").get(getSignup).post(postSignup);
+//with query
+// app.get('/user', )
+
+// app.post('/user', );
+
+// app.patch('/user', );
+
+// app.delete('/user', )
+
+//params
+// app.get('/user/:name', );
+function middleware1(req,res,next) {
+    console.log("midleware 1 called");
+    next();
+}
+
+// function middleware2(req,res) {
+//     console.log("midleware 2 called");
+//     res.json({ msg: "user returned" })
+// }
 
 async function getUsers(req, res) {
-  // console.log(req.query);
-  // let allUsers=await userModel.find();
-  let user=await userModel.findOne({name:'Abhishek'});
-  res.json({message:'list of all users',
-  data:allUsers});
+  console.log(req.query);
+  let { name, age } = req.query;
+  // let filteredData=user.filter(userObj => {
+  //     return (userObj.name==name && userObj.age==age)
+  // })
+  // res.send(filteredData);
+    
+    
+    //get all users from db
+    let allUsers=await userModel.findOne({name:"Abhishek"} )
+
+
+    res.json({ msg: "users retrieved", allUsers });
+    // console.log("getUser called ");
+    // next();
 }
 
 function postUser(req, res) {
-  console.log(req.body);
-  users = req.body;
+  console.log(req.body.Name);
+  //then i can put this in db
+  user.push(req.body);
   res.json({
-    message: "data received successfully",
+    message: "Data received successfully",
     user: req.body,
   });
 }
 
 async function updateUser(req, res) {
-  console.log("req.body-> ", req.body);
-  //update data in users obj
+  console.log(req.body);
   let dataToBeUpdated = req.body;
-  let user= await userModel.findOneAndUpdate({email:'abc@gmail.com'},dataToBeUpdated);
   // for (key in dataToBeUpdated) {
-  //   users[key] = dataToBeUpdated[key];
+  //   user[key] = dataToBeUpdated[key];
   // }
+  let doc = await userModel.findOneAndUpdate({ email: "abc@gmail.com" }, dataToBeUpdated);
   res.json({
-    message: "data updated successfully",
-    data:user
+    message: "data updated succesfully",
   });
 }
 
 async function deleteUser(req, res) {
-  // users = {};
-  let dataToBeDeleted=req.body;
-  let user=await userModel.findOneAndDelete(dataToBeDeleted);
+  // user = {};
+  // let doc = await userModel.deleteOne({ email: "abcd@gmail.com" });
+  // let doc = await userModel.findOneAndRemove({ email: "abcde@gmail.com" });
+  let user = await userModel.findOne({ email: "abc@gmail.com" });
+  console.log(user);
+  let del = await user.remove();
+  console.log(del);
   res.json({
-    message: "data has been deleted",
-    data:user
+    msg: "user has been deleted",
   });
 }
 
 function getUserById(req, res) {
-  console.log(req.params.username);
-    console.log(req.params);
-    res.send("user id received");
+  console.log(req.params.name);
+  //let {id}=req.params;
+  // let user = db.findOne(id);
+  res.json({ msg: "user id is ", obj: req.params });
 }
 
-function middleware1(req,res,next){
-  console.log('middleware1 encountered');
-  next();
+function getSignup(req, res) {
+    res.sendFile("/public/index.html", { root: __dirname });
 }
 
-function middleware2(req,res){
-  console.log('middleware2 encountered');
-  // next();
-  console.log("middleware 2 ended req/res cycle");
-  res.sendFile('/public/index.html',{root:__dirname});
+async function postSignup(req, res) {
+    // let { email, name, password } = req.body;
+    try {
+      let data = req.body;
+      console.log('in postsignup')
+        let user = await userModel.create(data);
+        console.log(data);
+        res.json({
+            msg: "user signed up",
+            user
+        })
+    }
+    catch (err) {
+        res.json({
+            err:err.message
+        })
+    }
 }
 
-function getSignUp(req,res,next){
-  console.log('getSignUp called');
-  next();
-  // res.sendFile('/public/index.html',{root:__dirname});
-}
-
-async function postSignUp(req,res){
-  let dataObj=req.body;
-  let user=await userModel.create(dataObj);
-  console.log('backend',user);
-  res.json({
-    message:"user signed up",
-    data:user
-  });
-}
+app.listen(9000);
 
 
-//mongoDB
 
-const db_link='mongodb+srv://backend_app:UKVM92aS7e9aENh7@cluster0.nvjh2w2.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(db_link)
-.then(function(db){
-   
-  console.log('db connected');
-})
-.catch(function(err){
-  console.log(err);
-});
-
-const userSchema=mongoose.Schema({
-  name:{
-    type:String,
-    required:true
-  },
-  email:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  password:{
-    type:String,
-    required:true,
-    minLength:8
-  },
-  confirmPassword:{
-    type:String,
-    required:true,
-    minLength:8
-  }
-});
-
-// model
-const userModel=mongoose.model('userModel',userSchema);
-
- (async function createUser(){
- let user1={
-      name:'1Jasbir',
-      email:'fvabcd@gmail.com',
-      password:'13112345678',
-      confirmPassword:'142412345678'
-  };
-    let data= await userModel.create(user1);
-    console.log(data);
- })();
+// (async function createUser() {
+//     let user = {
+//         name: "Rajesh",
+//         email: "xyz@gmail.com",
+//         password: "12345678",
+//         confirmPassword: "12345678"
+//     };
+//     let data = await userModel.create(user);
+//     console.log(data);
+// })();
